88cd080bb418107c801d0d7edc3165a9a7a5ab7f
John Foulkes <john.foulkes@onthebeach.co.uk>
2015-03-03 13:51:49 +0000

    Implemented triangle 2

--- the-language/lib/triangle.rb
+++ the-language/lib/triangle.rb
@@ -14,9 +14,23 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+	check_is_legal(a,b,c)
 	side_count_map = { 3 => :equilateral, 2 => :isosceles, 1 => :scalene }
 	total_sides_equal = equal_side_count(a, b, c)
 	side_count_map[total_sides_equal]
+end
+
+def check_is_legal(a,b,c)
+
+	sides = [a,b,c]
+	max = sides.max
+	other_sides = sides.reject { |side| side == max || side < 0 }
+	other_side_sum = other_sides.reduce(:+)
+
+	if !other_side_sum || max >= other_side_sum 
+		raise TriangleError
+	end
+
 end
 
 def equal_side_count(a, b, c)
@@ -27,4 +41,3 @@ end
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
-
